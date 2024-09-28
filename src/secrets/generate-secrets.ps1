e503d1a087a3cf0169274723dfca9c7792818bb0
MartinUggerholm <hrhoej@hotmail.com>
2024-09-28 10:33:48 +0200

    fix

--- src/secrets/generate-secrets.ps1
+++ src/secrets/generate-secrets.ps1
@@ -1,11 +1,10 @@
 . $PSScriptRoot/generate-secrets.functions.ps1
 
 $gitRoot = git rev-parse --show-toplevel
-$gitRoot2 = $gitRoot.Replace('C:/', '/mnt/c/')
-
+$gitRoot2 = $gitRoot.Replace('C:/', '/mnt/host/c/')
  
-Set-SealedSecrets -vault "mst-cew-preprod" -env " udv" -outputpath "$gitRoot/environments/udv/cew-platform-udv/templates/secrets" -certpath "$gitRoot2/environments/udv/kubeseal.cer" 
+Set-SealedSecrets -vault "legends-legacy-dev" -env "dev" -outputpath "$gitRoot/environments/dev/ll-platform-dev/templates/secrets" -certpath "$gitRoot2/environments/dev/kubeseal.cer" 
 
-Set-SealedSecrets -vault "mst-cew-preprod" -env " test" -outputpath "$gitRoot/environments/test/cew-platform-Test/templates/secrets" -certpath "$gitRoot2/environments/test/kubeseal.cer"
+# Set-SealedSecrets -vault "mst-cew-preprod" -env " test" -outputpath "$gitRoot/environments/test/cew-platform-Test/templates/secrets" -certpath "$gitRoot2/environments/test/kubeseal.cer"
 
-Set-SealedSecrets -vault "mst-cew-preprod" -env "preprod" -outputpath "$gitRoot/environments/preprod/cew-platform-preprod/templates/secrets" -certpath "$gitRoot2/environments/preprod/kubeseal.cer" 
+# Set-SealedSecrets -vault "mst-cew-preprod" -env "preprod" -outputpath "$gitRoot/environments/preprod/cew-platform-preprod/templates/secrets" -certpath "$gitRoot2/environments/preprod/kubeseal.cer" 
